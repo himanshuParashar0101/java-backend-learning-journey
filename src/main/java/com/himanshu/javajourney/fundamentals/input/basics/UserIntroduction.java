2d1173afdb8f703c5c1c71f3f1aef3c5ac5c8300
Himanshu Parashar <himanshuparashar0101@gmail.com>
2026-07-16 22:37:23 +0530

    chore(input): add scanner learning exercises in progress

---
+++ src/main/java/com/himanshu/javajourney/fundamentals/input/basics/UserIntroduction.java
@@ -0,0 +1,13 @@
+package com.himanshu.javajourney.fundamentals.input.basics;
+
+import  java.util.Scanner;
+
+public class UserIntroduction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("hello" + name);
+        scanner.close();
+    }
+}
